c6db7a4de72d910cb580216a15e674d924b9cf76
Tania Ali <taniafali@gmail.com>
2019-05-22 23:17:17 -0700

    Add few changes to code

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,8 @@
-// var player ;
-// var gameState;
-// var winningCombinations;
-// var countOfturns;
-// var resetButton = document.querySelector('#reset');
-// var startButton = document.querySelector('#start');
-let player = 'X';
-let winningCombinations = {
+// Model
+let player = null;
+let gameState = [];
+let countOfturns = null;
+const winningCombinations = {
   row1: [0,1,2],
   row2: [3,4,5],
   row3: [6,7,8],
@@ -15,26 +12,18 @@ let winningCombinations = {
   diagL : [0,4,8],
   diagR :[2,4,6]
 };
-let countOfturns = 0;
-var boardSize = 3;
-var boardSquares = new Array(boardSize ** 2 );
-var gameState =  Array(boardSquares).fill(0);
-var arrayX = [];
-var arrayO = [];
-let resetState = function() {
+let arrayX = [];
+let arrayO = [];
+let setState = function() {
   player = 'X';
-  gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  let boardSize = 3;
+  gameState =  Array(boardSize ** 2 ).fill(0);
   countOfturns = 0;
   arrayX = [];
   arrayO = [];
-  $board = document.getElementById('table');
-  for (var i = 0; i < $board.children.length; i++) {
-    $board.children[i].innerHTML = `<button  style="height: 100px; width : 100px"onClick="clickHandler(${i})"></button>`
-  }
-  document.getElementById('winDiv').innerHTML = "";
 };
-// function to check for win
 
+// function to check for win
 function checkWinner(player) {
   for (var i = 0; i < gameState.length; i++) {
     if (player === 'X' && gameState[i] === 'X') {
@@ -60,19 +49,38 @@ function checkWinner(player) {
   return false;
 }
 
-var swapTurns = function() {
+let swapTurns = function() {
   if(player === 'X') {
     player = 'O';
   } else {
     player = 'X';
   }
 };
-document.getElementById('turn').innerHTML = 'Player ' + player + ' turn';
-var clickHandler = function(id) {
+
+// view
+let startButton = document.querySelector('#start');
+let resetButton = document.querySelector('#reset');
+
+// controllers
+startButton.addEventListener("click", function(){
+  setState();
+  document.getElementById('turn').innerHTML = 'Player ' + player + ' turn';
+});
+
+resetButton.addEventListener("click", function(){
+  setState();
+  document.getElementById('turn').innerHTML = 'Player ' + player + ' turn';
+  $board = document.getElementById('table');
+  for (var i = 0; i < $board.children.length; i++) {
+    $board.children[i].innerHTML = `<button  style="height: 100px; width : 100px"onClick="clickHandler(${i})"></button>`
+  }
+  document.getElementById('winDiv').innerHTML = "";
+});
+
+let clickHandler = function(id) {
   //change the player name to active player
-  //document.getElementById('turn').innerHTML = 'Player ' + player + ' turn';
-  // place x or o on the square with id
   document.getElementById(id).innerHTML = player;
+  // place x or o on the square with id
   document.getElementById(id).style.fontSize = '100px';
   document.getElementById(id).style.fontWeight = 'bold'
   swapTurns();
@@ -101,21 +109,4 @@ var clickHandler = function(id) {
   }
 };
 
-/*
-if button clicked {
-  if game is active && turn == X && gamestate of that button is blank {
-    set that position in gamestate to X
-    turn = O
-  } else if game is active && turn == O && gamestate of that button is blank {
-    set that position in gamestate to O
-    turn = X
-  }
-
-}
-check all combinations array to gamestate array
-tell who has won
-
-have replay button show up
-reset all the variables
-*/
 
