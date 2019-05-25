63ad86c3fd2a91da7b75e2c51ce1e936f6e6f703
Tania Ali <taniafali@gmail.com>
2019-05-24 22:39:18 -0700

    Refactor form to use a file picker

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,27 +2,31 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const csvReportGernerator = require('./model').csvReportGernerator;
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = 3000;
-const csvReportGernerator = require('./model').csvReportGernerator;
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(fileUpload());
 app.use(express.static('client'));
-//app.set('view engine', path.join(__dirname, 'client', 'index.html'));
 app.set('view engine', 'ejs');
 app.set('views', 'client');
+
+
 app.listen(port, () => {
   console.log(`server is listening on ${port}`);
 });
 app.post('/upload_json', (req, res) => {
-
-  var parsed = JSON.parse(req.body.json);
-  var result = csvReportGernerator(parsed);
-  res.render('index', { data: result});
-  res.end();
+  if (Object.keys(req.files).length === 0) {
+    res.status(400).send('No files were uploaded');
+  }
+  let myfile = req.files.myFile.data.toString('utf8');
+  var result = csvReportGernerator(JSON.parse(myfile));
+   res.render('index', { data: result});
+   res.end();
 });
 
