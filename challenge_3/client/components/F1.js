e7c900dc185646944c9a6c933cdfe02f7e887880
Tania Ali <taniafali@gmail.com>
2019-05-28 11:17:21 -0700

    Working on challenge_3

---
+++ challenge_3/client/components/F1.js
@@ -0,0 +1,88 @@
+import React,{Component} from "react";
+import ReactDOM from "react-dom";
+export class CheckoutForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      step: 1,
+      name:"",
+      email: "",
+      password: "",
+      shipping_line: "",
+      shipping_city: "",
+      shipping_state:"",
+      shipping_zip: "",
+      phoneNumber:"",
+      creditCard: "",
+      expiryDate: "",
+      CVV: "",
+      billingZip : ""
+    };
+  this.goToNext = this.goToNext.bind(this);
+  this.goToPrev = this.goToBack.bind(this);
+  }
+  goToNext() {
+    const {step} = this.state;
+    this.setState({step: step + 1});
+  }
+  goToPrev() {
+    const {step} = this.state;
+    this.setState({step: step - 1});
+  }
+  handleChange(field) {
+    return event => this.setState({[field]: event.target.value});
+  }
+  render() {
+    const { step } = this.state;
+    const {name, email, password, shipping_line, shipping_city, shipping_state, shipping_zip, phoneNumber, creditCard, expiryDate, CVV, billingZip} = this.state;
+    const values = {name, email, password, shipping_line, shipping_city, shipping_state, shipping_zip, phoneNumber, creditCard, expiryDate, CVV, billingZip}
+      if (step === 1) {
+        return (
+          <button
+          // goToNext = {this.goToNext}
+          // handleChange={this.handleChange}
+          >Proceed To Checkout</button>
+        )
+      } else if (step === 2) {
+        return (
+          <PersonalInfo
+          key="personal"
+          goToNext = {this.goToNext}
+          handleChange={this.handleChange}
+          values={values}
+          />
+        )
+      } else if (step === 3) {
+        return (
+          <ShippingInfo
+          goToNext = {this.goToNext}
+          handleChange={this.handleChange}
+          goToPrev={this.goToPrev}
+          values={values}
+          />
+        )
+      } else if (step === 4) {
+        return (
+          <BillingInfo
+          goToNext = {this.goToNext}
+          handleChange={this.handleChange}
+          goToPrev={this.goToPrev}
+          values={values}
+          />
+        )
+      } else if (step === 5) {
+        return (
+          <ConfirmInfo
+            goToPrev={this.goToPrev}
+            values={values}
+          />
+        )
+      } else {
+        return (
+          <h1>no step detected</h1>
+        )
+      }
+  }
+}
+export default CheckoutForm;
+
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
-
-
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
